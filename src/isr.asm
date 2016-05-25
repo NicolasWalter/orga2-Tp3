c16af81efa778cc8ae9c20e80421df44a7accb5e
Horacio Gomez <horaciogomez.1993@gmail.com>
2016-05-25 07:33:59 -0300

    added exceptions and interrupts

--- src/isr.asm
+++ src/isr.asm
@@ -21,17 +21,70 @@ extern sched_proximo_indice
 ;; Definición de MACROS
 ;; -------------------------------------------------------------------------- ;;
 
-msgInt1: db 'DIVIDI POR 0'
-msgIntLen1 equ $ - msgInt1
 %macro ISR 1
 global _isr%1
-
 _isr%1:
     mov eax, %1
-    imprimir_texto_mp msgInt1, msgIntLen1, 0x07, 10, 0
+    imprimir_texto_mp msgInt%1, msgIntLen%1, 0x07, 3, 0
     jmp $
-
 %endmacro
+
+msgInt0: db 'Divide error'
+msgIntLen0 equ $ - msgInt0
+
+msgInt1: db 'RESERVED'
+msgIntLen1 equ $ - msgInt1
+
+msgInt2: db 'Nonmaskable external interrupt'
+msgIntLen2 equ $ - msgInt2
+
+msgInt3: db 'Breakpoint'
+msgIntLen3 equ $ - msgInt3
+
+msgInt4: db 'Overflow'
+msgIntLen4 equ $ - msgInt4
+
+msgInt5: db 'BOUND Range Exceeded'
+msgIntLen5 equ $ - msgInt5
+
+msgInt6: db 'Invalid Opcode'
+msgIntLen6 equ $ - msgInt6
+
+msgInt7: db 'Device Not Available (No Math Coprocessor)'
+msgIntLen7 equ $ - msgInt7
+
+msgInt8: db 'Double Fault'
+msgIntLen8 equ $ - msgInt8
+
+msgInt9: db 'Coprocessor Segment Overrun'
+msgIntLen9 equ $ - msgInt9
+
+msgInt10: db 'Invalid TSS'
+msgIntLen10 equ $ - msgInt10
+
+msgInt11: db 'Segment Not Present'
+msgIntLen11 equ $ - msgInt11
+
+msgInt12: db 'Stack-Segment Fault'
+msgIntLen12 equ $ - msgInt12
+
+msgInt13: db 'General Protection'
+msgIntLen13 equ $ - msgInt13
+
+msgInt14: db 'Page Fault'
+msgIntLen14 equ $ - msgInt14
+
+msgInt16: db 'x87 FPU Floating-Point Error'
+msgIntLen16 equ $ - msgInt16
+
+msgInt17: db 'Alignment Check'
+msgIntLen17 equ $ - msgInt17
+
+msgInt18: db 'Machine Check'
+msgIntLen18 equ $ - msgInt18
+
+msgInt19: db 'SIMD Floating-Point Exception'
+msgIntLen19 equ $ - msgInt19
 
 ;;
 ;; Datos
